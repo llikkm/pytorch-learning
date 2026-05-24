1507d8f72545586ec4c1eeb6761ad432226369c5
llikkm <eyliaranay@gmail.com>
2026-05-24 20:19:37 +0800

    Initial commit: deep learning and pytorch basics

---
+++ 0-6-what_deep learning.py
@@ -0,0 +1,23 @@
+#turning data into numbers and finding patterns in numbers
+#ML is for finding patterns for anything can convert to digits
+#
+#if you can build a simple rule_based system, that doesn't require a machine learning, do that.
+#不要害怕发布未采用机器学习技术的产品-google
+#
+#goodfor：problems with long listings of rules
+#continually changing environments
+#discovering insights within large collections of data
+#badfor:expiainability(人类还无法解释机器学习原理)
+#errors unacceptable
+#have no much data
+#
+#neural network: data->matrixs(numbers)->neutral network->matrixes(nums)->understandable outputs
+#imput layer->hidden layer(perform math)->output layer 
+# 
+# learning:supervised learning, unsupervised&self_supervised learning, transfer learning,reinforcement learning
+#the beginning 2 kind uses data and labels//
+# the transfer uses ome already learned sort to train another model
+#
+# 
+# #
+
