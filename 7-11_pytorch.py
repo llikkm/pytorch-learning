ee14f987cdbe01fedb8fb4869fcd89b6c83e32c2
llikkm <eyliaranay@gmail.com>
2026-05-26 12:13:39 +0800

    common new update

---
+++ 7-11_pytorch.py
@@ -0,0 +1,13 @@
+#able to write fast deep learning code
+#able to access many pre_built learning models.
+#gives a whole stack of deep learning: preprocess data,model data,deploy model in application
+#now open source
+#
+
+#what is a TENSOR?
+#data proceed to digits this digits are tensors
+#
+#built a workflow with pytorch:
+#data->pick a pre trained model->fit model to data->evaulate->improve->save and reload
+#
+
